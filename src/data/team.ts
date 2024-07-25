5f09a0a6a8dcfdd5b442d692ed9c34faf25264fe
michael <mikecicc@gmail.com>
2024-07-25 18:15:45 -0400

    add team member and designer data

---
+++ src/data/team.ts
@@ -0,0 +1,50 @@
+export const team = [
+  {
+    name: 'Newell Nussbaumer',
+    role: 'Co-Founder',
+    image: '/headshots/newell-nussbaumer.jpg',
+    bio: '<p>Newell Nussbaumer is the founder of Buffalo Rising, and numerous iconic events in Buffalo, NY.  His latest venture fig™, is designed to help create a united fashion front for Buffalo.  Newell’s goal is to roll out much more than a fashion show.  He intends to co-build a movement, along with the fig™ Team, and anyone else that is looking to support the designers, creators and artisans that have been waiting for the fashion industry in Buffalo to emerge and gain the recognition that it deserves.</p>',
+  },
+  {
+    name: 'Julia Papiernik',
+    role: 'Co-Founder',
+    image: '/headshots/julia-papiernik.jpg',
+    bio: "<p>After completing my studies in fashion design at the Fashion Institute of Technology in New York, I decided to return to Buffalo. However, I encountered difficulties starting a fashion career in Buffalo that would allow me to utilize my fashion design degree. Drawing from my upbringing which was heavily focused in real estate, I instinctively entered into the field, but I’ve always maintained the creative spirit to design, sew, express myself through fashion and celebrate style.</p><p>The birth of fig™ has been a catalyst to re-energize my creative pursuits and inspired me to contribute to Buffalo's fashion scene. Over the years, I have cultivated relationships with fellow artists, event visionaries, and fashion designers who share the same passion.  I launched an intimate wear collection at fig™ 2023 called SCOTT free. </p><p>The fig™ team's synergy, combined with the energy of other pop-up fashion events, is set to ignite a fashion movement in Buffalo that we have all been eagerly anticipating. Together, we are poised to celebrate style, cultivate a vibrant fashion community, and shape Buffalo's future in the world of fashion.</p>",
+  },
+  {
+    name: 'Sarah Bohn',
+    role: 'Co-Founder',
+    image: '/headshots/sarah-bohn.jpg',
+    bio: "<p>Sarah is a seasoned professional with a diverse background in events and real estate. She spent eight years as a senior account manager for an events and experiential communication agency in Beijing, China, specializing in luxury automotive events for global brands, delivering exceptional experiences for discerning clients.</p><p>Upon returning to Buffalo six years ago, Sarah seamlessly transitioned her career to the real estate industry, bringing her strategic mindset and strong client-focused approach to the local market.</p><p>Driven by her unwavering passion for events and fashion, Sarah immediately connected with the fashion show concept. Her deep affection for Buffalo fuels her determination to contribute to the city's growth and showcase its vibrant culture. Sarah is fully committed to curating an extraordinary fashion show experience that authentically captures the city's unique character and promising future. With every project, her ultimate goal is to elevate Buffalo's fashion industry, celebrating its rich heritage while embracing emerging trends. Through her work, Sarah aspires to leave a lasting impact and create a legacy that resonates with both the local community and the broader fashion world.</p>",
+  },
+  {
+    name: 'Jim Kupczyk',
+    role: 'Co-Founder',
+    image: '/headshots/jim-kupczyk.jpeg',
+    bio: "<p>Founder of mindfulmarket.com, I've dedicated my efforts to championing businesses that aspire to elevate our world. Drawing inspiration from the visionary mission of fig™, I am committed to galvanizing Buffalo's fashion landscape by bridging the gap between local designers, manufacturers, and boutiques. In supporting fig™, I stand testament to the belief that through collective creativity and synergy, we can cultivate a flourishing fashion ecosystem.</p>",
+  },
+  {
+    name: 'Emma Storfer',
+    role: 'Team Member',
+    image: '/headshots/emma-storfer.jpeg',
+    bio: "<p>Emma Storfer, the visionary behind Sweet & Dirty Vintage, is a prominent figure in Buffalo's vibrant creative landscape and a cherished member of the 716 Community. Hailing from Buffalo herself, Emma's lifelong dedication to fashion, styling, and fostering a sense of unity among her fellow residents has solidified her reputation as a leading voice in the local fashion scene. With a keen eye for design and an unwavering commitment to authenticity, Emma fearlessly shares her candid opinions on all things fashion-related. Whether you seek expert guidance or an unfiltered perspective, Emma's insightful advice is guaranteed to leave an indelible impression.</p>",
+  },
+  {
+    name: 'Sarah Schaefer',
+    role: 'Team Member',
+    image: '/headshots/sarah-schaefer.jpg',
+    bio: '<p>Sarah Schaefer is currently studying Advertising and Public Relations, with a minor in Professional and Technical Writing, at the University of Tampa. Originally from Buffalo, New York, she is passionate about contributing to her hometown’s vibrant community through her internship with fig™.</p>At UT, Sarah has developed a strong interest in content creation, social media campaigns, and event planning. She aims to expand her knowledge and experience in the advertising and public relations world, gaining valuable insights that will shape her future career path and professional identity. Sarah is excited to bring her enthusiasm and creativity to fig™, contributing to the growth and success of the team.<p>',
+  },
+  {
+    name: 'Linda Schineller',
+    role: 'Team Member',
+    image: '/headshots/linda-schineller.png',
+    bio: '<p>For many decades, Buffalonians supported a host of unheralded fashion designers, who were part of the cultural landscape of the city. They were the unsung heroes who reflected the style and taste of a variety of ethnicities and religions. Central to neighborhoods, seamstresses and tailors not only cut, sewed and altered clothing, but designed stylish outfits for their clients. For life’s big events, elegant bridal stores often designed and made the bride’s gown and the bridal party’s dresses. Lining Fillmore Avenue for decades, Frances Bridal, Clara Milas and other shops were loved and respected by generations of devotees. Gary White, the famed hat maker, who is still creating today, added to the artistry of the Eastside from his shop on Broadway. It was also common for these local artisans to create First Communion dresses for the Christian tradition, prom dresses, and even costumes for children’s dance recitals. As a marketing executive with proven expertise in driving revenue by building innovative strategies for brand development, thought leadership, advertising, communications, special events, and community partnerships, I hope to bring attention to the new wave of fashion designers, helping them to achieve the recognition they deserve—in Buffalo and beyond.<p>',
+  },
+  {
+    name: 'Cannon Flowers',
+    role: 'Team Member',
+    image: '/headshots/cannon-flowers.jpg',
+    bio: '<p>Cannon enjoyed a long career with Texas Instruments Incorporated in Dallas, Texas.  Cannon served in an executive leadership capacity in corporate finance.  His assignments were in many parts of the world, including Japan, The Philippines, Italy, Germany, Taiwan, and Singapore.  After retiring from TI, Cannon moved to the non-profit sector where he served the next 10 years as Chief Executive Officer of Human Rights Initiative - an organization in Texas that provides legal and social services to people seeking asylum in the United States. In creating a new phase of life, in 2020 he and his husband relocated to Western New York – both are committed to community building to make significant positive transformation and growth for Buffalo’s historic period of renewed vibrancy and attractiveness.  Cannon sees fig™ as a powerful tool in the building infrastructure to support the resurge of Buffalo’s full potential in fashion.  We help preserve and promote diverse cultural expressions, craftsmanship, and traditional techniques by supporting the fashion industry.<p>',
+  },
+]
--- src/pages/designers.tsx
+++ src/pages/designers.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-import { ContactForm, Container, SiteFooter } from '~/components/'
+import { ContactForm, Container, PersonCard, SiteFooter } from '~/components/'
+import { designers } from '~/data/designers'
 
 export default function DesignersPage() {
   return (
@@ -8,6 +9,21 @@ export default function DesignersPage() {
       <div className="flex flex-col gap-y-10 md:gap-y-20 bg-white pt-32">
         <div className="mx-auto max-w-[1020px] w-full p-10 flex flex-col gap-y-4 justify-center items-center text-center">
           <h1 className="text-4xl text-center mb-8">Designer Showcase</h1>
+          <div className="flex flex-col gap-y-6">
+            <h2>Fig 2024 Lineup</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+              {designers.map((person) => (
+                <PersonCard
+                  key={person.name}
+                  person={person}
+                  // className="w-[150px]"
+                  aspectRatio="square"
+                  width={150}
+                  height={150}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Container>
