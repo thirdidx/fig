bc059c546cd4a6b0e0a30389a8e315bb355640f3
michael <mikecicc@gmail.com>
2024-08-06 11:58:33 -0400

    swap mobile hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,23 +39,16 @@ export default function Hero() {
           </video>
         </div>
       ) : (
-        <>
-          <Image
-            src="/hero-bg.jpg"
-            quality={80}
-            priority
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: 'auto' }} // optional
-            alt=""
-          />
-          <img
-            src="/fig-vol2.svg"
-            alt=""
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-36 lg:w-full h-auto max-w-[467px] z-[3]"
-          />
-        </>
+        <Image
+          src="/hero-mobile.jpg"
+          quality={80}
+          priority
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: 'auto' }} // optional
+          alt=""
+        />
       )}
     </div>
   )
