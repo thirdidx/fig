abbf73fa0c1c297808aea30351da6c631b3191c7
michael <mikecicc@gmail.com>
2024-08-20 09:36:08 -0400

    swap hero video and sponsor logo

--- src/components/SponsorHero.tsx
+++ src/components/SponsorHero.tsx
@@ -15,11 +15,11 @@ export default function SponsorHero() {
         alt=""
       />
       <img
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2] w-full max-w-[160px] md:max-w-[320px] lg:max-w-[400px]"
-        src="/sponsors/feat/fig-x-evans-bank-white-stack.png"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[3] w-full max-w-[160px] md:max-w-[320px] lg:max-w-[400px]"
+        src="/presented-by.png"
         alt=""
       />
-      <div className="absolute left-0 top-0 w-full h-full bg-black/20 z-[3]" />
+      <div className="absolute left-0 top-0 w-full h-full bg-black/20 z-[2]" />
     </div>
   )
 }
