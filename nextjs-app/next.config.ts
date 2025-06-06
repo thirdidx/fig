40c90c0b9f77c4c18fdad58042729b1541de8f82
michael <mikecicc@gmail.com>
2025-06-06 15:19:31 -0400

    add cms to studio redirect

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: "false",
   },
+  async redirects() {
+    return [
+      {
+        source: '/cms',
+        destination: 'https://figbuffalo.sanity.studio',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
